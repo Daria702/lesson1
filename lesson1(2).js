26c0f35b7b4908577ab17da7fce5fa62499082cf
Daria702 <122361465+Daria702@users.noreply.github.com>
2023-11-21 17:02:42 +0100

    Add files via upload

---
+++ lesson1(2).js
@@ -0,0 +1,5 @@
+let firstName = 'Daria';
+let middleName = 'Oleksandrivna';
+let lastName = 'Bohachenko';
+let person = firstName+' '+middleName+' '+lastName;
+console.log (person);
---
+++ lesson1(3).js
@@ -0,0 +1,11 @@
+
+let a = 100;
+let b = '100'; 
+let c = true;
+let d = typeof (a);
+let e = typeof (b);
+let f = typeof (c);
+
+console.log(d);
+console.log(e);
+console.log(f);
